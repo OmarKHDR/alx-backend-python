4dabccc1d3b0bdc303cad7c6395e8c82e5bfa69a
OmarKHDR <omarsliman37@gmail.com>
2024-10-14 08:11:45 +0300

    :message: :tuple[]:

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -7,7 +7,7 @@ def to_kv(k: str, v: typing.Union[int, float]) -> typing.Tuple[str, float]:
     '''
     Args:
         k: str
-		v: Union[int, float]
+        v: Union[int, float]
     Return:
         Tupel
     '''
