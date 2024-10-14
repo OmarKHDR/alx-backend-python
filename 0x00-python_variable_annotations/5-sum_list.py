3515a26a4bac2ef1e8cab152620974768fb77bfa
OmarKHDR <omarsliman37@gmail.com>
2024-10-14 06:58:53 +0300

    :message: :float -> sum:

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 '''sum_list -> input_list
 '''
+from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     '''hello world
     '''
     return sum(input_list)
--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
+from typing import List, Union
 ''' for checker
 ;;;
 '''
+from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: list[int | float]) -> int | float:
+def sum_mixed_list(mxd_lst: list[Union[int, float]]) -> float:
     ''' [pls] [send]
     '''
     return sum(mxd_lst)
